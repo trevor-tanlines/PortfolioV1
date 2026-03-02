f0a5c589604b48d623c298c5f29e827246628045
T <ttan20@my.bcit.ca>
2026-03-02 11:53:58 -0800

    added the data for my skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,23 @@
 import { motion } from 'framer-motion';
 
+const skills = [
+  {
+    category: 'Frontend',
+    color: '#cc0000',
+    items: ['HTML', 'CSS', 'JavaScript', 'React', 'Tailwind CSS']
+  },
+   {
+    category: 'Video Editing',
+    color: '#ff6b35',
+    items: ['Adobe Premiere Pro', 'Adobe After Effects', 'Adobe Illustrator/Photoshop (Assets)'],
+  },
+   {
+    category: 'Currently Learning',
+    color: '#e8e8e8',
+    items: ['Python'],
+  },
+]
+
 function Skills() {
   return (
     <section>
