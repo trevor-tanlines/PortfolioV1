d1b26a641fc7a07412609c23478a3b8ce12fbaad
T <ttan20@my.bcit.ca>
2026-03-11 22:23:40 -0700

    added the title for my skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,8 +20,36 @@ const skills = [
 
 function Skills() {
   return (
-    <section>
-      <h2>My Skills</h2>
+    <section className='w-full min-h-screen flex items-center justify-center px-20 py-20'>
+    <div className='max-w-4xl w-full flex flex-col gap-12'>
+
+      <motion.h2
+        className='text-5xl font-bold'
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y:0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+      >
+        
+        My <span style={{ color: '#cc0000' }}>Skills</span>
+      </motion.h2>
+        {/* The title area */}
+
+      <div className='flex flex-col gap-12'></div>
+    </div>
+
+      {skills.map((group, groupIndex) => (
+        <div key={groupIndex}>
+          <h3>{group.category}</h3>
+
+          <div>
+            {group.items.map((skill, skillIndex) => (
+              <span key={skillIndex}>{skill}</span>
+            ))}
+          </div>
+
+        </div>
+      ))}
     </section>
   )
 }
