0c89717cb47891dd58369db3bc912b629420d3b1
Trevor <ttan20@my.bcit.ca>
2026-03-15 22:37:26 -0700

    added the temp links for the skelly

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,35 @@
 import { motion } from "framer-motion";
+import { label, link } from "framer-motion/client";
+
+const contacts = [
+  {
+    label: "Email",
+    value: "Emailfornow@gmail.com",
+    link: "emailto:Emailfornow@gmail.com",
+  },
+  {
+    label: "Github",
+    value: "Github.com/filloutlater",
+    link: "https://github.com/filloutlater",
+  },
+  {
+    label: "LinkedIn",
+    value: "linkedin.com/in/filloutlater",
+    link: "https://linkedin.com/in/filloutlater",
+  },
+]
 
 function Contact() {
   return (
     <section>
       <h2>Contact Me!</h2>
+
+      {contacts.map((contact, index) => (
+        <div key={index}>
+          <p>{contact.label}</p>
+          <a href={contact.link}>{contact.value}</a>
+        </div>
+      ))}
     </section>
   )
 }
