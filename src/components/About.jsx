e70f1e03f68e6b19f2c7bd5bfa0aabdb30d84575
Trevor <ttan20@my.bcit.ca>
2026-03-16 03:41:29 -0700

    About Skelly, styling will come later

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,24 @@
 function About() {
   return (
     <section id="about" className="w-full min-h-screen flex items-center justify-center px-20 py-20">
+      <div className="max-w-4xl w-full flex flex-col gap-12">
+
+        <h2>About <span>Me</span></h2>
+
+        <div>
+          <p>
+            Hello to whoever's reading, I'm Trevor, a frontend dev student and video editor, 
+            I enjoy making things that look and feel great to use and experience.
+          </p>
+          <p>
+            Using core design principles as a base, I build and create projects that pique interests and leave a lasting impression.
+          </p>
+          <p>
+            This portfolio is a look into my works and what I have to offer.
+          </p>
+        </div>
+
+      </div>
       <h1>About ME</h1>
     </section>
   )
