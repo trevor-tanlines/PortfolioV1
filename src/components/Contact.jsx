df1a27b97eed416334318d2b7182164d02ed41bf
Trevor <ttan20@my.bcit.ca>
2026-03-16 02:13:24 -0700

    Some animations for the contact sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,15 +23,29 @@ function Contact() {
     <section className="w-full min-h-screen flex items-center justify-center px-20 py-20">
       <div className="max-w-4xl w-full flex flex-col gap-12">
 
-<h2 className="text-5xl font-bold">
+<motion.h2 className="text-5xl font-bold"
+  initial={{ opacity: 0, y:30 }}
+  whileInView={{ opacity: 1, y: 0}}
+  transition={{ duration: 0.3 }}
+  viewport={{ once: true }}
+>
   Contact <span style={{color: '#cc0000' }}>Me</span>!
-</h2>
+</motion.h2>
 {/* Title */}
 
 
     <div className="mt-8 flex flex-col gap-6">
       {contacts.map((contact, index) => ( 
-        <div key={index} className="flex flex-col gap-1 border-l-2 pl-4" style={{borderColor: '#cc0000'}}> {/* border thickness  and colour */}
+        <motion.div key={index} 
+        className="flex flex-col gap-1 border-l-2 pl-4"
+        style={{ borderColor: '#cc0000' }}
+        initial={{ opacity: 0, x: -30 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.3, delay:index * 0.15 }}
+        viewport={{ once: true }}
+        whileHover={{ x:6 }}
+        > {/* border thickness  and colour */}
+        
           <span className="text-sm text-gray-500">{contact.label}</span>
 
           <a href={contact.link}
@@ -40,7 +54,7 @@ function Contact() {
           rel="noreferrer"
           >
           {contact.value}</a>
-        </div>
+        </motion.div>
       ))}
     </div>
       </div>
